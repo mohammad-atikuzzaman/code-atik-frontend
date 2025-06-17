df4300f161b7a5e27939917becaf8fb4e40607ad
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-06-17 22:39:51 +0600

    base url of login and registed modified

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export const registerUser = (data: { email: string; password: string }) =>
